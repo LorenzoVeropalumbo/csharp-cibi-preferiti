4073566c3c6b53d49f8a4664479615acbab602e0
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-10-27 10:09:51 +0200

    creazione della base

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// crezione dell'array dei cibi preferiti
+string[] cibiPreferiti = { "carbonara", "pizza", "sushi", "patatine fritte", "pancake" };
+
+//stampo la lunghezza dell'array
+Console.WriteLine(cibiPreferiti.Length + " questa è la lunghezza della classifica dei cibi");
+
+//stampo l'array come una classifica
+for(int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(i+")" + cibiPreferiti[i]);
+}
+
+//stampo la prima posizione
+Console.WriteLine(cibiPreferiti[0] + " questa è il mio cibo preferito");
+
+//stampo l' ultima posizione
+Console.WriteLine(cibiPreferiti[1] + " questa è il mio cibo preferito");
